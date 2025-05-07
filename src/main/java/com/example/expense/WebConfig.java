25ac902db3ca4a4d65cefccaccbe886411c89665
ASHTAD IRANI <ashtadcyrusirani@gmail.com>
2025-05-07 11:01:14 +0530

    changed webconfig

--- src/main/java/com/example/expense/WebConfig.java
+++ src/main/java/com/example/expense/WebConfig.java
@@ -1,24 +1,45 @@
-package com.example.expense;
+//package com.example.expense;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//		    	registry.addMapping("/**")
+//		        .allowedOrigins(
+//		            "https://symphonious-otter-07cb0e.netlify.app",
+//		            "https://dev--symphonious-otter-07cb0e.netlify.app"
+//		        )
+//        		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowCredentials(true)
+//                .allowedHeaders("Authorization", "Content-Type")
+//         		.exposedHeaders("Set-Cookie");     
+//        
+//         
+//    }
+//}
 
+package com.example.expense;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-		    	registry.addMapping("/**")
-		        .allowedOrigins(
-		            "https://symphonious-otter-07cb0e.netlify.app",
-		            "https://dev--symphonious-otter-07cb0e.netlify.app"
-		        )
-        		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .allowedHeaders("Authorization", "Content-Type")
-         		.exposedHeaders("Set-Cookie");     
-        
-         
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "https://symphonious-otter-07cb0e.netlify.app",
+                "https://dev--symphonious-otter-07cb0e.netlify.app"
+            )
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
+
